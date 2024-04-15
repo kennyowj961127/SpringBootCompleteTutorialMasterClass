86ba86c3da703009f8bafdadf8a97b1bbc64a049
kennyowj961127 <123232019+kennyowj961127@users.noreply.github.com>
2024-04-15 23:12:28 +0800

    XML Dependency Injection Example

---
+++ SpringFrameworkDemo/src/main/java/org/example/Doctor.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Doctor implements Staff{
+
+
+    private String qualification;
+
+    private Nurse nurse;
+
+    public void assist() {
+        System.out.println("Doctor is assisting");
+    };
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public Nurse getNurse() {
+        return nurse;
+    }
+
+    public void setNurse(Nurse nurse) {
+        this.nurse = nurse;
+    }
+}
