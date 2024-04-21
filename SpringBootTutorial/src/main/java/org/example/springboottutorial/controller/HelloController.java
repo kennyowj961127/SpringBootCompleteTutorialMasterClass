66de687f1313e847797e636685f1560bff32eb47
kennyowj961127 <123232019+kennyowj961127@users.noreply.github.com>
2024-04-21 11:09:53 +0800

    Using application properties value

--- SpringBootTutorial/src/main/java/org/example/springboottutorial/controller/HelloController.java
+++ SpringBootTutorial/src/main/java/org/example/springboottutorial/controller/HelloController.java
@@ -1,5 +1,6 @@
 package org.example.springboottutorial.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController()
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomMessage;
+
     @GetMapping("/")
     public String hello() {
-        return "Hello, World!!!";
+        return welcomMessage;
     }
 
 }
