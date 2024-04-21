790bffc6d42ab7ae4c97fd071906026fa0123b49
kennyowj961127 <123232019+kennyowj961127@users.noreply.github.com>
2024-04-21 20:58:19 +0800

    Init JPA tutorial folder

---
+++ spring-data-jpa-tutorial/src/test/java/org/example/springdatajpatutorial/SpringDataJpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springdatajpatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
